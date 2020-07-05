d22a27170288d0fc3c7efa7e3b60eaa94a98f6f0
Sachin Saini <iamsainisachin@gmail.com>
2020-07-05 19:22:45 +0530

    test lru

---
+++ src/main.go
@@ -0,0 +1,39 @@
+// Copyright (C) 2020 Sachin Saini
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import "fmt"
+
+func main() {
+	lru := NewLru(12)
+	val := lru.Get("sachin")
+	fmt.Println(val)
+	fmt.Println(lru.size)
+	lru.Set("sachin", "saini")
+	val = lru.Get("sachin")
+	fmt.Println(val)
+	fmt.Println(lru.size)
+	lru.Set("peeyush", "pessss")
+	val = lru.Get("peeyush")
+	fmt.Println(val)
+	fmt.Println(lru.size)
+	lru.Set("vrinda", "ves")
+	val = lru.Get("vrinda")
+	fmt.Println(val)
+	fmt.Println(lru.size)
+	val = lru.Get("sachin")
+	fmt.Println(val)
+}
