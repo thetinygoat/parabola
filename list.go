547ea3f7ff729a7d1f30e760347e4afea1003bcb
Sachin Saini <iamsainisachin@gmail.com>
2020-07-21 19:03:21 +0530

    implement list ds

--- list.go
+++ list.go
@@ -15,37 +15,96 @@
 
 package main
 
-import dll "github.com/emirpasic/gods/lists/doublylinkedlist"
+import (
+	dll "github.com/emirpasic/gods/lists/doublylinkedlist"
+)
 
-// List contains data related to a particular list
+const ()
+
+// List contains data related to a list
+// list - is a pointer the the list data structre
+// mem - is the memory footprint of the list
 type List struct {
-	list    *dll.List
-	memSize uint64
+	list *dll.List
+	mem  uint64
 }
 
-// ListSpace contains all the lists
-type ListSpace struct {
-	listSpace map[string]*List
-	listCount uint64
+// NewList instantiates a new list
+func NewList() *List {
+	l := List{list: dll.New(), mem: 0}
+	return &l
 }
 
-// InitListSpace initializes the listspace
-func InitListSpace() *ListSpace {
-	ls := ListSpace{}
-	ls.listCount = 0
-	ls.listSpace = make(map[string]*List)
-	return &ls
+// allocate allocates virtual memory from the mempool
+func (l *List) allocate(data string) uint64 {
+	allocSize := uint64(len(data))
+	l.mem += allocSize
+	return allocSize
 }
 
-// Append appends data to the specified list
-func (ls *ListSpace) Append(listName, data string) {
-	_, exists := ls.listSpace[listName]
-	if !exists {
-		newList := List{list: dll.New(), memSize: 0}
-		ls.listSpace[listName] = &newList
+// free frees the virtual memory
+func (l *List) free(data string) uint64 {
+	if l.mem == 0 {
+		return 0
 	}
+	deallocSize := uint64(len(data))
+	l.mem -= deallocSize
+	return deallocSize
+}
 
-	list, _ := ls.listSpace[listName]
-	list.list.Append(data)
+// Append appends data to the list
+func (l *List) Append(data string) bool {
+	l.list.Append(data)
+	return true
+}
 
+// Prepend prepends data to the list
+func (l *List) Prepend(data string) bool {
+	l.list.Prepend(data)
+	return true
+}
+
+// RemoveLast removes last element from the list
+func (l *List) RemoveLast() (string, bool) {
+	size := l.list.Size()
+	data, exists := l.list.Get(size - 1)
+	if !exists {
+		return "", false
+	}
+	l.list.Remove(size - 1)
+	return data.(string), true
+}
+
+// RemoveFirst removes first element from the list
+func (l *List) RemoveFirst() (string, bool) {
+	data, exists := l.list.Get(0)
+	if !exists {
+		return "", false
+	}
+	l.list.Remove(0)
+	return data.(string), true
+}
+
+// Length returns the length of the list
+func (l *List) Length() int {
+	return l.list.Size()
+}
+
+// Get returns element at any index
+func (l *List) Get(index int) (string, bool) {
+	data, exists := l.list.Get(index)
+	if !exists {
+		return "", false
+	}
+	return data.(string), true
+}
+
+// Remove removes element from any index
+func (l *List) Remove(index int) (string, bool) {
+	data, exists := l.list.Get(index)
+	if !exists {
+		return "", false
+	}
+	l.list.Remove(index)
+	return data.(string), true
 }
