abbd46d7ce77896397e74e79aa4e5a4fdd9b6ca1
Sachin Saini <iamsainisachin@gmail.com>
2020-08-26 14:29:51 +0530

    add flags for port and memory

--- main.go
+++ main.go
@@ -14,7 +14,12 @@
 
 package main
 
+import "flag"
+
 func main() {
-	srv, _ := NewServer("8080", 102400)
+	port := flag.String("port", "9898", "Port to run DictX server on")
+	maxMemory := flag.Int64("mem", 1073741824, "Max memory limit for the DictX server")
+	flag.Parse()
+	srv, _ := NewServer(*port, *maxMemory)
 	srv.Listen()
 }
